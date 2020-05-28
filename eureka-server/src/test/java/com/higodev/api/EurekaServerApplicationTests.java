2a441c2ad0f36c6714954e4cc2a733f866db83d7
Higo Alexandre <higoalexandreb@gmail.com>
2020-05-27 22:08:56 -0300

    Added Service Discovery Eureka

---
+++ eureka-server/src/test/java/com/higodev/api/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.higodev.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
--- store/src/main/java/com/higodev/api/services/BuyService.java
+++ store/src/main/java/com/higodev/api/services/BuyService.java
@@ -1,5 +1,6 @@
 package com.higodev.api.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -10,11 +11,13 @@ import com.higodev.api.controllers.dtos.InfoProviderDto;
 
 @Service
 public class BuyService {
+	
+	@Autowired
+	private RestTemplate client;
 
 	public void makeBuy(BuyDto buyDto) {
-		RestTemplate client = new RestTemplate();
 		ResponseEntity<InfoProviderDto> exchange = client.exchange(
-				"http://localhost:8081/info/" + buyDto.getAddress().getState(), HttpMethod.GET, null,
+				"http://provider/info/" + buyDto.getAddress().getState(), HttpMethod.GET, null,
 				InfoProviderDto.class);
 		System.out.println("Fornecedor eh de " + exchange.getBody().getState());
 	}
